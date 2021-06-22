121fe191f9d8358ff69dc9ca223e6a0706705257
Hugo Gonçalves <Hugo1307@users.noreply.github.com>
2021-06-22 01:20:01 +0100

    Added workspace details screen.

---
+++ BD_FinalProject/WorkspaceDetails.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace BD_FinalProject
+{
+    partial class WorkspaceDetails
+    {
+        /// <summary> 
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Designer de Componentes
+
+        /// <summary> 
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lb_WorkspaceName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Lbx_WorkspaceUsers = new System.Windows.Forms.ListBox();
+            this.Btn_AddTransaction = new System.Windows.Forms.Button();
+            this.Lb_WorkspaceAdmin = new System.Windows.Forms.Label();
+            this.Lb_WorkspaceCreationDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Oswald SemiBold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(173)))), ((int)(((byte)(68)))));
+            this.label1.Location = new System.Drawing.Point(37, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 46);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Workspace";
+            // 
+            // Lb_WorkspaceName
+            // 
+            this.Lb_WorkspaceName.AutoSize = true;
+            this.Lb_WorkspaceName.BackColor = System.Drawing.Color.Transparent;
+            this.Lb_WorkspaceName.Font = new System.Drawing.Font("Oswald Medium", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Lb_WorkspaceName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(173)))), ((int)(((byte)(68)))));
+            this.Lb_WorkspaceName.Location = new System.Drawing.Point(38, 96);
+            this.Lb_WorkspaceName.Name = "Lb_WorkspaceName";
+            this.Lb_WorkspaceName.Size = new System.Drawing.Size(140, 35);
+            this.Lb_WorkspaceName.TabIndex = 4;
+            this.Lb_WorkspaceName.Text = "First Workspace";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Oswald SemiBold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(38, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Workspace Admin: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Oswald SemiBold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(37, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Workspace creation date:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Oswald SemiBold", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(38, 268);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Workspace Users:";
+            // 
+            // Lbx_WorkspaceUsers
+            // 
+            this.Lbx_WorkspaceUsers.BackColor = System.Drawing.Color.White;
+            this.Lbx_WorkspaceUsers.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Lbx_WorkspaceUsers.Font = new System.Drawing.Font("Oswald", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Lbx_WorkspaceUsers.FormattingEnabled = true;
+            this.Lbx_WorkspaceUsers.ItemHeight = 22;
+            this.Lbx_WorkspaceUsers.Location = new System.Drawing.Point(40, 309);
+            this.Lbx_WorkspaceUsers.Name = "Lbx_WorkspaceUsers";
+            this.Lbx_WorkspaceUsers.Size = new System.Drawing.Size(293, 220);
+            this.Lbx_WorkspaceUsers.TabIndex = 8;
+            // 
+            // Btn_AddTransaction
+            // 
+            this.Btn_AddTransaction.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(173)))), ((int)(((byte)(68)))));
+            this.Btn_AddTransaction.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(173)))), ((int)(((byte)(68)))));
+            this.Btn_AddTransaction.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Btn_AddTransaction.Font = new System.Drawing.Font("Oswald", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Btn_AddTransaction.ForeColor = System.Drawing.Color.White;
+            this.Btn_AddTransaction.Location = new System.Drawing.Point(37, 562);
+            this.Btn_AddTransaction.Name = "Btn_AddTransaction";
+            this.Btn_AddTransaction.Size = new System.Drawing.Size(245, 42);
+            this.Btn_AddTransaction.TabIndex = 17;
+            this.Btn_AddTransaction.Text = "Delete Workspace";
+            this.Btn_AddTransaction.UseVisualStyleBackColor = false;
+            // 
+            // Lb_WorkspaceAdmin
+            // 
+            this.Lb_WorkspaceAdmin.AutoSize = true;
+            this.Lb_WorkspaceAdmin.BackColor = System.Drawing.Color.Transparent;
+            this.Lb_WorkspaceAdmin.Font = new System.Drawing.Font("Oswald", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Lb_WorkspaceAdmin.ForeColor = System.Drawing.Color.Black;
+            this.Lb_WorkspaceAdmin.Location = new System.Drawing.Point(148, 179);
+            this.Lb_WorkspaceAdmin.Name = "Lb_WorkspaceAdmin";
+            this.Lb_WorkspaceAdmin.Size = new System.Drawing.Size(128, 22);
+            this.Lb_WorkspaceAdmin.TabIndex = 18;
+            this.Lb_WorkspaceAdmin.Text = "hugogoncalves13@ua.pt";
+            // 
+            // Lb_WorkspaceCreationDate
+            // 
+            this.Lb_WorkspaceCreationDate.AutoSize = true;
+            this.Lb_WorkspaceCreationDate.BackColor = System.Drawing.Color.Transparent;
+            this.Lb_WorkspaceCreationDate.Font = new System.Drawing.Font("Oswald", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Lb_WorkspaceCreationDate.ForeColor = System.Drawing.Color.Black;
+            this.Lb_WorkspaceCreationDate.Location = new System.Drawing.Point(187, 216);
+            this.Lb_WorkspaceCreationDate.Name = "Lb_WorkspaceCreationDate";
+            this.Lb_WorkspaceCreationDate.Size = new System.Drawing.Size(69, 22);
+            this.Lb_WorkspaceCreationDate.TabIndex = 19;
+            this.Lb_WorkspaceCreationDate.Text = "21/06/2021";
+            // 
+            // WorkspaceDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.Lb_WorkspaceCreationDate);
+            this.Controls.Add(this.Lb_WorkspaceAdmin);
+            this.Controls.Add(this.Btn_AddTransaction);
+            this.Controls.Add(this.Lbx_WorkspaceUsers);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Lb_WorkspaceName);
+            this.Controls.Add(this.label1);
+            this.Name = "WorkspaceDetails";
+            this.Size = new System.Drawing.Size(1268, 669);
+            this.Load += new System.EventHandler(this.WorkspaceDetails_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Lb_WorkspaceName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox Lbx_WorkspaceUsers;
+        private System.Windows.Forms.Button Btn_AddTransaction;
+        private System.Windows.Forms.Label Lb_WorkspaceAdmin;
+        private System.Windows.Forms.Label Lb_WorkspaceCreationDate;
+    }
+}
---
+++ BD_FinalProject/WorkspaceDetails.cs
@@ -0,0 +1,58 @@
+﻿using BD_FinalProject.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BD_FinalProject
+{
+    public partial class WorkspaceDetails : UserControl, Route
+    {
+
+        private int workspaceID;
+
+        public int WorkspaceID { get => workspaceID; set => workspaceID = value; }
+
+        public WorkspaceDetails(int workspaceID)
+        {
+            InitializeComponent();
+            this.workspaceID = workspaceID;
+        }
+        private void WorkspaceDetails_Load(object sender, EventArgs e)
+        {
+            DBCommander dBCommander = DBCommander.getInstance();
+            Workspace workspace = dBCommander.getWorkspaceByID(this.workspaceID);
+            List<User> workspaceUsers = dBCommander.getWorkspaceUsers(this.workspaceID);
+
+            if (workspace != null)
+            {
+                Lb_WorkspaceName.Text = workspace.Name;
+                Lb_WorkspaceAdmin.Text = workspace.Admin;
+                Lb_WorkspaceCreationDate.Text = workspace.CreationDate.ToString();
+                foreach (User user in workspaceUsers)
+                {
+                    Lbx_WorkspaceUsers.Items.Add(user.Name + "(" + user.Email + ")");
+                }
+               
+            }
+
+        }
+
+        public RouteName getName()
+        {
+            return RouteName.WORKSPACE_DETAILS;
+        }
+
+        public UserControl getNewInstance()
+        {
+            return new WorkspaceDetails(1);
+        }
+
+        
+    }
+}
